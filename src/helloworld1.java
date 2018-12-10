a565de5dfae310231605f81e166c3d0bdd05d48e
zj <759444285@qq.com>
2018-12-10 12:37:44 +0800

    修改

--- src/helloworld1.java
+++ src/helloworld1.java
@@ -1,8 +1,6 @@
-public class helloworld {
-
+public class helloworld1 {
     public static void main(String[] args){
         System.out.println("11111");
         System.out.println("成功提交到git");
     }
 }
-
